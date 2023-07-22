51e9d1af22ca43b6de6f3b2a2f5f9d793c60d408
Hartaithan <hartaithan@gmail.com>
2023-07-22 13:29:28 +0900

    add colors and spacing styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,8 +3,6 @@ import { StyleSheet } from 'react-native';
 const global = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 });
 
---
+++ src/styles/spacing.ts
@@ -0,0 +1,24 @@
+const defaultSpacing = 8;
+
+export const spacing = {
+  /** 0px */
+  0: defaultSpacing * 0,
+  /** 2px */
+  1: defaultSpacing * 0.25,
+  /** 4px */
+  2: defaultSpacing * 0.5,
+  /** 8px */
+  3: defaultSpacing,
+  /** 12px */
+  4: defaultSpacing * 1.5,
+  /** 16px */
+  5: defaultSpacing * 2,
+  /** 24px */
+  6: defaultSpacing * 3,
+  /** 32px */
+  7: defaultSpacing * 4,
+  /** 48px */
+  8: defaultSpacing * 6,
+  /** 64px */
+  9: defaultSpacing * 8,
+};
